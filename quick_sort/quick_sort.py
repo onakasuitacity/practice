a7293eb1f57a15365da1dec063bfbd53e467a5b6
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2020-02-11 08:02:26 +0900

    Update quick_sort.py

--- quick_sort/quick_sort.py
+++ quick_sort/quick_sort.py
@@ -13,13 +13,12 @@ def quick_sort(A, l=0, r=len(A)-1):
         if i>=j: break
         A[i], A[j] = A[j], A[i]
         i+=1; j-=1
-    # divide
-    if i-1-l<=r-j-1:
+    # divide (with tail call)
+    if i-l<=r-j:
         quick_sort(A, l, i-1)
-        quick_sort(A, j+1, r)
+        return quick_sort(A, j+1, r)
     else:
         quick_sort(A, j+1, r)
-        quick_sort(A, l, i-1)
-    return A
+        return quick_sort(A, l, i-1)
 
 print(quick_sort(A))
