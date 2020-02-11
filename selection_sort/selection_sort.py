e92f9bec04fb830ecd3360ac779924fd563d30e0
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2020-02-11 23:37:06 +0900

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -2,10 +2,7 @@ A = [3, 8, 12, 25, 6, 23, 7, 1, 15, 4, 9, 17]
 
 n = len(A)
 for i in range(n-1):
-    now, idx = A[i], i
-    for j in range(i+1,n):
-        if now > A[j]:
-            now, idx = A[j], j
-    A[i], A[idx] = A[idx], A[i]
+    j = min((A[k],k) for k in range(i,n))[1]
+    A[i], A[j] = A[j], A[i]
 
 print(A)
