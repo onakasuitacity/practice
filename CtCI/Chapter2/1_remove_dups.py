ce7078c41f1ae8bfdbda2fa7659517da6e584400
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2020-02-15 08:24:33 +0900

    Update 1_remove_dups.py

--- CtCI/Chapter2/1_remove_dups.py
+++ CtCI/Chapter2/1_remove_dups.py
@@ -1,22 +1,18 @@
 from LinkedList import LinkedList
 
 def delete_duplication(ll):
-    prev = ll.front
-    if prev is None:
-        return
-    now = prev.next
+    now = ll.front
     if now is None:
         return
+    used = set()
+    used.add(now.value)
 
-    S = set()
-    S.add(prev.value)
-    while now is not None:
-        if now.value in S:
-            now, prev.next = now.next, now.next
+    while now.next is not None:
+        if now.next.value in used:
+            now.next = now.next.next
         else:
-            S.add(now.value)
-            prev, now = now, now.next
-
+            used.add(now.next.value)
+            now = now.next
 
 ll = LinkedList()
 ll.generate(20,0,5)
