7a42fcb996c3d97ea5ce4b2e1fd7d7774c777abe
OnakaSuitaCity <54064820+onakasuitacity@users.noreply.github.com>
2020-02-16 06:08:27 +0900

    Update 2_return_kth_to_last.py

--- CtCI/Chapter2/2_return_kth_to_last.py
+++ CtCI/Chapter2/2_return_kth_to_last.py
@@ -1,12 +1,12 @@
 from LinkedList import LinkedList
 
 # recursion
-def kth_node(node, k):
+def kth_node1(node, k):
     if node is None:
         return 0
 
-    res = kth_node(node.next, k)
-    if isinstance(res,int):
+    res = kth_node1(node.next, k)
+    if isinstance(res, int):
         if res < k-1:
             return res + 1
         else:
@@ -15,20 +15,20 @@ def kth_node(node, k):
         return res
 
 # iterate
-def kth_node(ll, k):
-    kth_next = ll.front
+def kth_node2(ll, k):
+    kth_next = ll.front()
     for _ in range(k):
         if kth_next is None:
             return None
         kth_next = kth_next.next
 
-    now = ll.front
+    now = ll.front()
     while kth_next:
         now, kth_next = now.next, kth_next.next
     return now
 
 ll = LinkedList()
-ll.generate(10,0,7)
+ll.generate(10)
 print(ll)
-# print(kth_node(ll.front, 2))
-print(kth_node(ll, 2))
+print(kth_node1(ll.front(), 3))
+print(kth_node2(ll, 3))
